637a0b38bb3574c5276a910a866867f9e2816f91
jpedrorl <jpedrorl@gmail.com>
2014-08-03 00:10:08 -0300

    Merged Interlude

--- PlatformPrototype/src/states/GameStateTest.java
+++ PlatformPrototype/src/states/GameStateTest.java
@@ -6,6 +6,8 @@
 
 package states;
 
+import code.Animation;
+import code.TimeBar;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.event.MouseEvent;
@@ -22,20 +24,33 @@ public class GameStateTest extends GameState{
     // private int oldMouseX, oldMouseY;
     Interlude interlude;
 
+    Animation animaBarney;
     @Override
     public void init() {
+    timebar = new TimeBar(0, 0, 7);
+    animaBarney = new Animation(10, "background_time_bar");
+    animaBarney.addPointInTime("background_time_bar", 0, new Point(2, 2));
+    animaBarney.addPointInTime("background_time_bar", 4, new Point(100,100));
+    animaBarney.addPointInTime("background_time_bar", 6, new Point(400, -50));
+    animaBarney.addPointInTime("background_time_bar", 7, new Point(0, 1000));
+    
     	String[] strings = new String[0];	
     	interlude = new Interlude(strings, new Point(100,100), 2, 10, 0);
     }
 
     @Override
     public void update(long wait) {  
+    timebar.update(wait);
+   animaBarney.updateFrame(wait);
+    
     	interlude.update(wait);
     }
 
     @Override
     public void draw(Graphics2D g) {
-    	// Imprimir coisas na tela =D
+    	timebar.draw(g);
+   animaBarney.drawFrame(g);
+   
     	interlude.draw(g);
     }
 
