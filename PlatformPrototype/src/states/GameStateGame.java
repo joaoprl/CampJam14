44e54f998a58407fa796e0fa3cd460b30440cfc2
peronio <henriquelalves@gmail.com>
2014-08-02 23:20:37 -0300

    Animation in GameStateGame

--- PlatformPrototype/src/states/GameStateGame.java
+++ PlatformPrototype/src/states/GameStateGame.java
@@ -23,11 +23,21 @@ public class GameStateGame extends GameState{
     public static final int PLAYER_CHOICE_ABSTAIN        = 0;
     public static final int PLAYER_CHOICE_HELP           = 1;
     public static final int PLAYER_CHOICE_DONTHELP       = 2;
+    public static final int TIME_LIMIT                  = 10;
     private int[] players_choices;
     private boolean inputEnable;
+    private long choiceTime;
     
     // Current substate
+    public static final int STATE_PROLOGUE               = 0;
+    public static final int STATE_SCENE1                 = 1;
+    public static final int STATE_SCENE2                 = 2;
+    public static final int STATE_SCENE3                 = 3;
+    public static final int STATE_SCENE4                 = 4;
+    public static final int STATE_SCENE5                 = 5;
+    public static final int STATE_EPILOGUE               = 6;
     private int currentState;
+    private boolean inScene;
     private Scene gameScene;
     private Interlude gameInterlude;
     
@@ -45,19 +55,51 @@ public class GameStateGame extends GameState{
             i = PLAYER_CHOICE_ABSTAIN;
         }
         
+        // First state: prologue!
         inputEnable = false;
+        currentState = STATE_PROLOGUE;
+        gameScene = new Scene(sceneAnimations(currentState));
+        inScene = true;
     }
 
     @Override
     public void update(long wait) {
+        if(inScene){
+            gameScene.update(wait);
+            if(gameScene.isOver() && !inputEnable){
+                if(currentState != STATE_EPILOGUE && currentState != STATE_PROLOGUE){
+                    if(!inputEnable){
+                        // Enabling input, next state is Interlude
+                        inputEnable = true;
+                        choiceTime = TIME_LIMIT;
+                    } else {
+                        choiceTime -= wait;
+                        // Update Timebar
+                        if(choiceTime < 0){
+                            for(int i: players_choices){
+                                i = i == PLAYER_CHOICE_ABSTAIN ? PLAYER_CHOICE_DONTHELP : i;
+                            }
+                            nextState();
+                        }
+                    }
+                } else {
+                    nextState();
+                }
+
+            }
+        } else {
+            // interlude.update()
+        }
     }
 
     @Override
     public void draw(Graphics2D g) {
+        gameScene.draw(g);
     }
 
     @Override
     public void keyPressed(int k) {
+        
     }
 
     @Override
@@ -85,29 +127,36 @@ public class GameStateGame extends GameState{
         //lel
     }
     
-    enum SCENE_ANIMATIONS{
-        PROLOGUE,SCENE1,SCENE2,SCENE3,SCENE4,SCENE5,EPILOGUE;
-        
-        public Animation getAnimation(){
-            Animation newAnimation = null;
-            switch(this){
-                case PROLOGUE:
-                
-                    break;
-                case SCENE1:
-                    break;
-                case SCENE2:
-                    break;
-                case SCENE3:
-                    break;
-                case SCENE4:
-                    break;
-                case SCENE5:
-                    break;
-                case EPILOGUE:
-                    break;
-            }
-            return newAnimation;
+    private void nextState(){
+        if(inScene){
+            // new Interlude
+        } else {
+            
         }
+        
+    }
+    
+    private Animation[] sceneAnimations(int i){
+        Animation[] newAnimation = null;
+        switch(i){
+            case STATE_PROLOGUE:
+                break;
+            case STATE_SCENE1:
+                break;
+            case STATE_SCENE2:
+                break;
+            case STATE_SCENE3:
+                break;
+            case STATE_SCENE4:
+                break;
+            case STATE_SCENE5:
+                break;
+            case STATE_EPILOGUE:
+                break;
+            default:
+                //lel
+                break;
+        }
+        return newAnimation;
     }
 }
